bcbd9bba5dfff4789cdf602c30990fc2a112f2fb
Tofayel Choudhury <tofayel659@gmail.com>
2019-05-31 17:40:17 +0100

    Add files via upload

---
+++ Strados/NavTab.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title> Navigation tab</title>
+    <link rel="stylesheet" href="NavBackground.css">
+    
+     <link rel="stylesheet" href="navResponsive.js">
+    <body>
+        
+        
+        
+   
+        <table>
+            <tr>
+    
+             <th></th>
+                <th> <div class ="topdropdown">
+                    
+                     <div class="dropdown">
+  <button class="dropbtn">Sign in ▼</button>
+  <div class="dropdown-content">
+    <a href="#Candiate_Sign in">Sign in as Candidate</a>
+    <a href="#Client sign in">Sign in as Client</a>
+  </div>
+</div>
+                 <a href="#">
+           <img src = "images/linkdin.png" width="30px" height="30px" >
+               </a>
+                    
+            <a href="#">
+           <img src = "images/twitter.png" width="30px" height="30px" >
+               </a>    
+                    
+                    
+                    
+                    
+                    </div>
+                    
+                    
+                </th>
+               
+            </tr>
+           
+                
+                <tr>
+            
+            
+         <td> <div class="topnavleft">
+       
+       <img src="LogoStrados.jpg" height="100" width="180"> </div> </td>
+            
+              
+        
+          <td>
+              
+              <div class ="topdropdown">
+                    
+                     <div class="dropdown">
+  <button class="dropbtn" >Register ▼</button>
+  <div class="dropdown-content">
+    <a href="#">Regiser as Candidate</a>
+    <a href="#">Register as Client</a>
+  </div>
+</div>
+                 
+                    </div> 
+              <div class="topnav" id="myTopnav">
+              
+
+                
+                    
+                    
+                    
+                   
+              
+
+ <a href="#">Contact Us</a>
+            
+    
+              
+<a href="#news">Strados Community</a>
+                
+                
+                
+  <a href="#contact">Who We Are</a>
+              
+              
+  <a href="#news" class="active">Opportunities</a>
+ 
+
+      
+              </div>
+                   
+                    
+                    </td>
+                    
+            
+                    
+                
+            </tr>
+    
+    <tr>
+
+<td  colspan="2">
+
+    <div class="topmenu">
+    
+    </div>
+    
+    
+</td>
+            
+            
+            
+        </table>
+
+        
+         
+        
+           
+                 
+        
+       
+        
+    </body>
+    </head>
+
+</html>
---
+++ Strados/index.php
@@ -0,0 +1,144 @@
+<?php
+require ('NavTab.php');  
+
+?>
+
+ <!-- The About US Section -->
+
+    <html>
+    <link rel="stylesheet" href="master.css">
+        <link rel ="stylesheet" href="imgback.css">
+  
+     <div class="w3-bi">
+  <div  class=" w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="aboutus">
+   
+      
+      
+      
+      
+  <!--   <img src="images/LogoStrados.jpg" style="width:100%"> -->
+      
+    <h2 class="w3-wide ">About Us</h2>
+      <div class="w3-container  w3-white">
+    <p class="w3-opacity"><i>EXPERTS IN PROVIDING TELENT</i></p>
+    <p class="w3-justify">We have created a Recruitment firm. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+   </div>   
+</div>
+    
+
+  <!-- The Our Expertise Section -->
+    
+  <div class="w3-orange" id="ourexpertise">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">OUR EXPERTISE</h2>
+      <p class="w3-opacity w3-center"><i>EXPERTS IN PROVIDING TALENT</i></p><br>
+
+      <ul class="w3-ul w3-border w3-white w3-text-grey">
+        <li class="w3-padding">
+            <p>
+            At Strados we provide a dedicated team where they will offer you, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra eros at tellus efficitur, nec scelerisque sem sagittis. Proin at venenatis orci. Morbi ut tortor ut mauris eleifend scelerisque. Curabitur quis sapien a ipsum viverra consequat sit amet non purus. Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales.
+            
+            
+            </p>  
+          
+          
+          
+          </li>
+        
+      </ul>
+
+        
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+         
+          <div class="w3-container w3-white">
+            <p><b>Candiates</b></p>
+            <p class="w3-opacity">Morbi ut tortor ut mauris eleifend scelerisque. Curabitur quis sapien a ipsum viverra consequat sit amet non purus. Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales.</p>
+           
+           
+          </div>
+        </div>
+          
+         
+        <div class="w3-third w3-margin-bottom">
+         
+          <div class="w3-container w3-white">
+            <p><b>Our Clients</b></p>
+            <p class="w3-opacity">Curabitur quis sapien a ipsum viverra consequat sit amet non purus. Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales.</p>
+            
+           
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+         
+          <div class="w3-container w3-white">
+            
+              <p><b>Employment </b></p>
+            <p class="w3-opacity">Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales. Curabitur quis sapien a ipsum viverra consequat sit amet non purus. </p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- This is the Testimonial Section -->
+    <div class="w3-bi2">
+    <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="testimonials">
+      
+      
+    <h2 class="w3-wide">Testimonials</h2>
+    <p class="w3-opacity"><i>EXPERTS IN PROVIDING TELENT</i></p>
+    <div class="w3-container w3-white">
+            
+              <p><b>George Svilen </b></p>
+            <p class="w3-opacity">Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales. Curabitur quis sapien a ipsum viverra consequat sit amet non purus. </p>
+        
+        
+        <p><b>Matthew Everson</b></p>
+            <p class="w3-opacity">Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales. Curabitur quis sapien a ipsum viverra consequat sit amet non purus. </p>
+        
+        <p><b>Manpreet Singh </b></p>
+            <p class="w3-opacity">Integer molestie tincidunt eleifend. Praesent tincidunt lacus vel lacus eleifend sodales. Curabitur quis sapien a ipsum viverra consequat sit amet non purus. </p>
+            
+          </div>
+
+  </div>
+    </div>
+<!-- The Contact Section -->
+ <div class="w3-orange" id="contactus">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+    <h2 class="w3-wide w3-center">CONTACT US</h2>
+    <p class="w3-opacity w3-center"><i>EXPERTS IN PROVIDING TALENT</i></p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-col m6 w3-large w3-margin-bottom">
+        <i class="fa fa-map-marker" style="width:30px"></i> Enfield, London<br>
+        <i class="fa fa-phone" style="width:30px"></i> Contact Number: 02083456789<br>
+        <i class="fa fa-envelope" style="width:30px"> </i> Email: STRADOS@Hotmail.com<br>
+      </div>
+      <div class="w3-col m6">
+        <form action="/action_page.php" target="_blank">
+          <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+            </div>
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+            </div>
+          </div>
+          <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
+          <button class="w3-button w3-orange w3-section w3-right" type="submit">SEND</button>
+        </form>
+      </div>
+    </div>
+  </div>
+    </div>
+  </html>
+<!-- End Page Content -->
+<?php
+require ('footer.php'); 
+
+?>
